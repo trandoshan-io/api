155eec3da72c7c82da6df613033eb2abc51f62e0
Aloïs Micard <lunamicard@gmail.com>
2019-09-15 19:41:53 +0200

    Add endpoint to retrieve page content

--- api.go
+++ api.go
@@ -28,6 +28,7 @@ type PageData struct {
 	Id        primitive.ObjectID `bson:"_id"`
 	Url       string             `bson:"url"`
 	CrawlDate time.Time          `bson:"crawlDate"`
+	Content   string             `bson:"content"`
 }
 
 func main() {
@@ -53,6 +54,7 @@ func main() {
 
 	// Register endpoints
 	router.HandleFunc("/pages", searchPagesHandler(client)).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/pages/{page-id}/content", viewPageContentHandler(client)).Methods(http.MethodGet, http.MethodOptions)
 	router.HandleFunc("/count-pages", countPagesHandler(client)).Methods(http.MethodGet, http.MethodOptions)
 
 	// todo add websocket endpoint
@@ -100,7 +102,37 @@ func searchPagesHandler(client *mongo.Client) func(w http.ResponseWriter, r *htt
 			return
 		}
 
+		// Write json response
 		if err := httputil.WriteJsonResponse(w, 200, pages); err != nil {
+			log.Println("Error while writing response to client: " + err.Error())
+		}
+	}
+}
+
+func viewPageContentHandler(client *mongo.Client) func(w http.ResponseWriter, r *http.Request) {
+	pageCollection := client.Database("trandoshan").Collection("pages")
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		// Get page ID
+		pageId, err := primitive.ObjectIDFromHex(mux.Vars(r)["page-id"])
+		if err != nil {
+			log.Println("Unable to convert objectId from hex value: " + err.Error())
+			return
+		}
+
+		// Setup production context
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+		// Query database for result
+		var page PageData
+		if err := pageCollection.FindOne(ctx, bson.M{"_id": pageId}).Decode(&page); err != nil {
+			log.Println("Error while decoding result: " + err.Error())
+			return
+		}
+
+		// Write json response
+		if err := httputil.WriteJsonResponse(w, 200, page.Content); err != nil {
 			log.Println("Error while writing response to client: " + err.Error())
 		}
 	}
